a8099f9c28b4c66c654e295966a483a52975827b
Branislav Marton <branislav.marton@mikroe.com>
2018-08-30 11:30:55 +0200

    Update bsp

--- pic32/bsp.c
+++ pic32/bsp.c
@@ -11,33 +11,33 @@ typedef struct
 bsp_gpio_t _mikrobus_map[MIKROBUS_COUNT][MIKROBUS_PIN_COUNT] = {
     /* MIKROBUS_1 */
     {
-        /* MIKROBUS_AN */ {PORTB, PIN8},
-        /* MIKROBUS_RST */ {PORTC, PIN1},
-        /* MIKROBUS_CS */ {PORTC, PIN2},
-        /* MIKROBUS_SCK */ {PORTB, PIN8},
-        /* MIKROBUS_MISO */ {PORTB, PIN8},
-        /* MIKROBUS_MOSI */ {PORTB, PIN8},
-        /* MIKROBUS_PWM */ {PORTD, PIN0},
-        /* MIKROBUS_INT */ {PORTB, PIN8},
-        /* MIKROBUS_RX */ {PORTB, PIN8},
-        /* MIKROBUS_TX */ {PORTB, PIN8},
-        /* MIKROBUS_SCL */ {PORTB, PIN8},
-        /* MIKROBUS_SDA */ {PORTB, PIN8}
+        {PORTB, PIN8}, /* MIKROBUS_AN */
+        {PORTC, PIN1}, /* MIKROBUS_RST */
+        {PORTC, PIN2}, /* MIKROBUS_CS */
+        {PORTB, PIN8}, /* MIKROBUS_SCK */
+        {PORTB, PIN8}, /* MIKROBUS_MISO */
+        {PORTB, PIN8}, /* MIKROBUS_MOSI */
+        {PORTD, PIN0}, /* MIKROBUS_PWM */
+        {PORTB, PIN8}, /* MIKROBUS_INT */
+        {PORTB, PIN8}, /* MIKROBUS_RX */
+        {PORTB, PIN8}, /* MIKROBUS_TX */
+        {PORTB, PIN8}, /* MIKROBUS_SCL */
+        {PORTB, PIN8}  /* MIKROBUS_SDA */
     },
     /* MIKROBUS_2 */
     {
-        /* MIKROBUS_AN */ {PORTB, PIN8},
-        /* MIKROBUS_RST */ {PORTC, PIN1},
-        /* MIKROBUS_CS */ {PORTC, PIN2},
-        /* MIKROBUS_SCK */ {PORTB, PIN8},
-        /* MIKROBUS_MISO */ {PORTB, PIN8},
-        /* MIKROBUS_MOSI */ {PORTB, PIN8},
-        /* MIKROBUS_PWM */ {PORTD, PIN0},
-        /* MIKROBUS_INT */ {PORTB, PIN8},
-        /* MIKROBUS_RX */ {PORTB, PIN8},
-        /* MIKROBUS_TX */ {PORTB, PIN8},
-        /* MIKROBUS_SCL */ {PORTB, PIN8},
-        /* MIKROBUS_SDA */ {PORTB, PIN8}
+        {PORTB, PIN8}, /* MIKROBUS_AN */
+        {PORTC, PIN1}, /* MIKROBUS_RST */
+        {PORTC, PIN2}, /* MIKROBUS_CS */
+        {PORTB, PIN8}, /* MIKROBUS_SCK */
+        {PORTB, PIN8}, /* MIKROBUS_MISO */
+        {PORTB, PIN8}, /* MIKROBUS_MOSI */
+        {PORTD, PIN0}, /* MIKROBUS_PWM */
+        {PORTB, PIN8}, /* MIKROBUS_INT */
+        {PORTB, PIN8}, /* MIKROBUS_RX */
+        {PORTB, PIN8}, /* MIKROBUS_TX */
+        {PORTB, PIN8}, /* MIKROBUS_SCL */
+        {PORTB, PIN8}  /* MIKROBUS_SDA */
     }
 };
 
@@ -54,29 +54,17 @@ void bsp_mikrobus_init_digital(mikrobus_dir_t dir, mikrobus_t bus, mikrobus_pin_
 void bsp_mikrobus_set(mikrobus_t bus, mikrobus_pin_t pin)
 {
     bsp_gpio_t *pinout = &_mikrobus_map[bus][pin];
-    if (bus > MIKROBUS_COUNT - 1)
-        return;
-    if (pin > MIKROBUS_PIN_COUNT - 1)
-        return;
     hal_gpio_write(pinout->port, pinout->pin, 1);
 }
 
 void bsp_mikrobus_rst(mikrobus_t bus, mikrobus_pin_t pin)
 {
     bsp_gpio_t *pinout = &_mikrobus_map[bus][pin];
-    if (bus > MIKROBUS_COUNT - 1)
-        return;
-    if (pin > MIKROBUS_PIN_COUNT - 1)
-        return;
     hal_gpio_write(pinout->port, pinout->pin, 0);
 }
 
 void bsp_mikrobus_toggle(mikrobus_t bus, mikrobus_pin_t pin)
 {
     bsp_gpio_t *pinout = &_mikrobus_map[bus][pin];
-    if (bus > MIKROBUS_COUNT - 1)
-        return;
-    if (pin > MIKROBUS_PIN_COUNT - 1)
-        return;
     hal_gpio_toggle(pinout->port, pinout->pin);
 }
